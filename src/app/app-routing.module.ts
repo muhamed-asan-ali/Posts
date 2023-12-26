199dd9551abfb68623a00f960973e2662051bca2
muhamed-asan-ali <asanasanali0@gmail.com>
2023-12-26 06:49:46 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLoginComponent } from './components/user-login/user-login.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { PublicPageComponent } from './components/public-page/public-page.component';
+import { PersonalPageComponent } from './components/personal-page/personal-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: UserLoginComponent },
+  { path: 'login', component: UserLoginComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'profile', component: PersonalPageComponent  },
+  { path: 'public-feed', component: PublicPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,58 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { PublicPageComponent } from './components/public-page/public-page.component';
+import { PersonalPageComponent } from './components/personal-page/personal-page.component';
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { CardModule } from 'primeng/card';
+import { PanelModule } from 'primeng/panel';
+import { DialogModule } from 'primeng/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { AvatarModule } from 'primeng/avatar';
+import { NgOptimizedImage } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLoginComponent,
+    HomePageComponent,
+    PublicPageComponent,
+    PersonalPageComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    MenuModule,
+    OverlayPanelModule,
+    CardModule,
+    PanelModule,
+    DialogModule,
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgOptimizedImage,
+    CardModule,
+    AvatarModule,
+    AvatarGroupModule,
+    InputTextModule,
+    ButtonModule
   ],
-  providers: [],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
+  providers: [ provideClientHydration() ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
